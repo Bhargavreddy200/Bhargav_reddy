df6367680f777c1eb63d9d2d040eb10ddc0b3704
Bhargav Reddy <Bhargav Reddy@DESKTOP-P3PM2MK>
2025-11-17 11:06:50 +0530

    bhargav commit

---
+++ tap_programs/src/tap_academy/data_types.java
@@ -0,0 +1,25 @@
+package tap_academy;
+
+public class data_types {
+
+	public static void main(String[] args) {
+		
+		/*float a = (float)45.6;
+		System.out.println(a);
+	
+		//int a= 10;
+		float d = 25.5f;
+		double b = 5;
+		b=d;
+		System.out.println(d);
+		System.out.println(b);*/
+		
+		//int a = 45;
+		float g= 35.8f;
+		byte t;
+		t=(byte)g;
+		System.out.println(t);
+		
+	}
+
+}
