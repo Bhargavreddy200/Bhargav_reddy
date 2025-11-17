306e417bfc2f6e34c546e2fbeb89fa9ddda22c2c
Bhargav Reddy <Bhargav Reddy@DESKTOP-P3PM2MK>
2025-11-17 11:17:20 +0530

    bhargav commit

---
+++ tap_programs/src/veriables/javapractice.java
@@ -0,0 +1,23 @@
+package veriables;
+
+public class javapractice {
+	int id;
+	String name;
+	float salary;
+	
+	
+	public static void main(String[] args) {
+		
+		javapractice jj = new javapractice();
+	jj.id=101;
+	jj.name="baby";
+	jj.salary=2999;
+	System.out.println(jj.id);
+	System.out.println(jj.name);
+	System.out.println(jj.salary);
+	
+
+	
+	}
+
+}
